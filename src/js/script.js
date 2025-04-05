8fa2cba53805582c042170bac2cc0c4d62fc3b37
Lucas <lucasdealmeidamack@gmail.com>
2025-04-05 07:01:12 -0300

    adicionando exemplo03 dia 04-04-25

---
+++ src/js/script.js
@@ -0,0 +1,104 @@
+// //DECLARARAÇÕES E VARIAVEIS
+
+// var nome ="fiap";
+// console.log(nome);
+
+// let idade = 19;
+// console.log(idade)
+
+// const sobrenome="Fiapinho";
+// console.log(sobrenome)
+
+// //declarando uma variavel indefinida
+// let nome1;
+// console.log(nome1)
+
+// //declarando uma variavel nula
+// let nome2=null;
+// console.log(nome2)
+
+// //TIPOS DE VARIAVEIS
+
+// let exemplo1 =10;
+// console.log(typeof exemplo1)
+
+// let exemplo2 ="Tecnologia";
+// console.log(typeof exemplo2)
+
+// let exemplo3 =true;
+// console.log(typeof exemplo3)
+
+// let exemplo4={};
+// console.log(typeof exemplo4)
+
+// let exemplo5 =[];
+// console.log(typeof exemplo5)
+
+
+//CONVERSÕES
+
+//FLOAT => STRING
+
+// let numFloat =123.456;
+// console.log(numFloat.toString())
+
+// //STRING => FLOAT
+// let numString ="12.963";
+// console.log(parseFloat(numString))
+
+// //INT => STRING
+// let numInt =100;
+// console.log(numInt.toString())
+
+// //STRING => INT
+
+// let numString1 ="120";
+// console.log(parseInt(numString1))
+
+//METODOS
+
+//METODO length - VERIFICA O TAMNHO DA STRING
+
+// let frase ="O mundo da Tecnologia";
+// console.log(frase.length)
+
+// //METODO Indeof/lastIndexOf-RETORNA UM TRECHO DO SEU CODIGO
+
+// let texto ="Programação Sustentável ão";
+// console.log(texto.lastIndexOf("ão"))
+
+// //METODO slice - RETORNA PARTE DE UM TEXTO PASSANDO INICIO E O FINAL
+// let info ="Programação de ponta";
+// console.log(info.slice(14,20))
+
+//OPERADORES ARITMÉTICOS
+
+// const a=10;
+// const b=20;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+//OPERADORES LÓGICOS
+
+// const c=20;
+// const d=30;
+
+// console.log(c > d);
+// console.log(c < d && c < 10);
+// console.log(c > d || d > c);
+// console.log(c == d || d <= c )
+
+//OPERADORES DE COMPARAÇÃO
+// let e="10";
+// let f="10";
+
+// console.log( e == f);
+// console.log( e === f);
+// console.log( e != f);
+
+
+//CONDICIONAL 
+
